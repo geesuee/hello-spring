c3922de57f8d08400c7b35e851f153d29c5c17e9
geesuee <baejisu97@gmail.com>
2022-10-24 22:49:04 +0900

    2022/10/24 복습

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,49 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody   // http 통신 body 부에 return 값을 직접 넣어주겠다는 의미
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // html 을 찾아 내려주는 템플릿 엔진 방식과 다르게 값 (=데이터)만을 보내줌, 소스보기 했을 때 값만 있음
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
